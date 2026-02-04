8bbf6e3943e608e75c61d90166b9e6b81c7e58ca
Sebastian <sebastianpersonal244@gmail.com>
2026-02-04 09:33:16 -0500

    Crear clase CuentaBancaria

---
+++ src/CuentaBancaria.java
@@ -0,0 +1,27 @@
+public class CuentaBancaria {
+    private final String id;
+
+    private int saldo;
+
+
+    public CuentaBancaria(String id, int saldoInicial) {
+        this.id = id;
+        this.saldo = saldoInicial;
+
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+
+}
